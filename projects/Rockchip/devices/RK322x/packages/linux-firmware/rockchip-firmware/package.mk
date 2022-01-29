edc2d71f0887ce04ba799030f456aeb33397393e
ilmich <ardutu@gmail.com>
2022-01-29 18:13:16 +0100

    rk322x: in the beginning

---
+++ projects/Rockchip/devices/RK322x/packages/linux-firmware/rockchip-firmware/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="rockchip-firmware"
+PKG_VERSION="firmware"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/rockchip-linux"
+PKG_URL=""
+PKG_DEPENDS_TARGET="rkbin"
+PKG_LONGDESC="rockchip firmware"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_firmware_dir)/ssv6051
+    cp -v $(get_build_dir rkbin)/firmware/wifi/ssv6051-sw.bin $INSTALL/$(get_full_firmware_dir)/
+    cp -v $(get_build_dir rkbin)/firmware/wifi/ssv6051-wifi.cfg $INSTALL/$(get_full_firmware_dir)/
+}
---
+++ projects/Rockchip/devices/RK322x/packages/tools/rkbin/package.mk
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="rkbin"
+
+# Version is: knaerzche/tag:libreelec-991d0e4
+PKG_VERSION="6490e59afa3341501350d23b42c0138f41724d33"
+PKG_SHA256="f3cb2787e7d2b9701f7ad19564428c19337ce818ec541072501a4c1e96d89677"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/rockchip-linux/rkbin"
+PKG_URL="https://github.com/ilmich/rkbin/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
+PKG_TOOLCHAIN="manual"
+
