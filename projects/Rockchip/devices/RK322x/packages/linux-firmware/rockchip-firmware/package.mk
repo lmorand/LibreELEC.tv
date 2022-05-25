f81643ea168b291ed1de4e92dabcdaf33f7c84dc
ilmich <ardutu@gmail.com>
2022-05-25 19:11:54 +0200

    rk322x: added rtl8723cs bluetooth fw

--- projects/Rockchip/devices/RK322x/packages/linux-firmware/rockchip-firmware/package.mk
+++ projects/Rockchip/devices/RK322x/packages/linux-firmware/rockchip-firmware/package.mk
@@ -12,7 +12,10 @@ PKG_LONGDESC="rockchip firmware"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/$(get_full_firmware_dir)/ssv6051
+  mkdir -p $INSTALL/$(get_full_firmware_dir)/{ssv6051,rtl_bt}
     cp -v $(get_build_dir rkbin)/firmware/wifi/ssv6051-sw.bin $INSTALL/$(get_full_firmware_dir)/
     cp -v $(get_build_dir rkbin)/firmware/wifi/ssv6051-wifi.cfg $INSTALL/$(get_full_firmware_dir)/
+    cp -v $(get_build_dir rkbin)/firmware/bluetooth/rtl8723cs_xx_config.bin $INSTALL/$(get_full_firmware_dir)/rtl_bt
+    cp -v $(get_build_dir rkbin)/firmware/bluetooth/rtl8723cs_xx_fw.bin $INSTALL/$(get_full_firmware_dir)/rtl_bt
+    
 }
